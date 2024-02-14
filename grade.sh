eaea57014422efd56598deafbd4388bc5bff02d2
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-02-14 15:24:59 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,11 +1,11 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission # rm -rf recursively removes files and directories
 rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission 2> ta-output.txt
 echo 'Finished cloning'
 
 
@@ -23,15 +23,33 @@ cd grading-area
 
 if ! [ -f ListExamples.java ] # -f checks if ListExamples.java exists and is a file
 then
-    echo "Missing ListExamples.java in student submission"
-    echo "Score: 0"
-    exit
+	echo "Missing ListExamples.java in student submission"
+	echo "Score: 0"
+	exit 1
 fi
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+javac -cp $CPATH *.java
 if [ $? -ne 0 ] # -ne is numeric inequality operator
 then
-    echo "Compilation Error"
-    echo "Score: 0"
-    exit
+	echo "Compilation Error"
+	echo "Score: 0"
+	exit 1
 fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+
+if [ $? -ne 0 ] # -ne is numeric inequality operator
+then
+	tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+	failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+	exit 1
+else
+	tests=$(echo $lastline | awk -F'[()]' '{print $2}' | awk -F'[ ]' '{print $1}')
+	failures=0
+	exit
+fi
+
+successes=$((tests - failures))
+echo "Your score is $successes / $tests"
