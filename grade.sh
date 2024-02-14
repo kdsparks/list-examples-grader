70fb507f30b5cf80dc58ad0e29e1263a4488c42b
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-02-14 10:16:03 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,6 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-rm -rf student-submission
+rm -rf student-submission # rm -rf recursively removes files and directories
 rm -rf grading-area
 
 mkdir grading-area
@@ -14,3 +14,24 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp student-submission/*.java grading-area
+cp *.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [ -f ListExamples.java ] # -f checks if ListExamples.java exists and is a file
+then
+    echo "Missing ListExamples.java in student submission"
+    echo "Score: 0"
+    exit
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+if [ $? -ne 0 ] # -ne is numeric inequality operator
+then
+    echo "Compilation Error"
+    echo "Score: 0"
+    exit
+fi
